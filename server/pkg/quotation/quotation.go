3cbb52812cb09753ce0356c644775c7f9b247733
Felipe Azevedo <felipe.azevedo@cws.digital>
2025-03-12 19:25:07 -0300

    adjust types to unmarshall values correctly

--- server/pkg/quotation/quotation.go
+++ server/pkg/quotation/quotation.go
@@ -3,6 +3,7 @@ package quotation
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -11,6 +12,10 @@ import (
 )
 
 type QuotationResponse struct {
+	USDBRL USDBRL `json:"USDBRL"`
+}
+
+type USDBRL struct {
 	Code       string `json:"code"`
 	Codein     string `json:"codein"`
 	Name       string `json:"name"`
@@ -25,34 +30,34 @@ type QuotationResponse struct {
 }
 
 func GetUSDBRLQuotation(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(getenv.QuotationApiTimeout))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(getenv.QuotationApiTimeout*uint64(time.Millisecond)))
 	defer cancel()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, getenv.QuotationApiEndpoint, nil)
 	if err != nil {
-		http.Error(w, "error while creating a request with context", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("error while creating a request with context: %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
 
 	response, err := http.DefaultClient.Do(req)
 	if err != nil {
-		http.Error(w, "error when making the request to the quotation api", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("error when making the request to the quotation api: %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		http.Error(w, "error while reading the body response", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("error while reading the body response: %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
 
 	var quotationResponse QuotationResponse
 	err = json.Unmarshal(body, &quotationResponse)
 	if err != nil {
-		http.Error(w, "error while unmarshalling the body response into go struct", http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("error while unmarshalling the body response into go struct: %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
 
-	w.Write([]byte(quotationResponse.Bid))
+	w.Write([]byte(quotationResponse.USDBRL.Bid))
 }
