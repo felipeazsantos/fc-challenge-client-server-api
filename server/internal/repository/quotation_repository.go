1a047be26c4f6cb6f864f16372aad6115bb182a5
felipeazsantos <felipeasantos10@hotmail.com>
2025-03-21 12:56:26 -0300

    insert quotation on sqlite

--- server/internal/repository/quotation_repository.go
+++ server/internal/repository/quotation_repository.go
@@ -14,8 +14,37 @@ func (q *QuotationRepository) InsertQuotation(quotation *model.USDBRL) error {
 		return err
 	}
 
-	stmt, err := db.Prepare(`INSERT INTO quotation()`)
+	stmt, err := db.Prepare(`INSERT INTO quotation(code, code_in, name, high, low, var_bid, pct_change, bid, ask, timestamp, create_date)
+							 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return err
+	}
 
+	defer func() error {
+		err = stmt.Close()
+		if err != nil {
+			return err
+		}
+		return nil
+	}()
+
+	_, err = stmt.Exec(
+		quotation.Code,
+		quotation.Codein,
+		quotation.Name,
+		quotation.High,
+		quotation.Low,
+		quotation.VarBid,
+		quotation.PctChange,
+		quotation.Bid,
+		quotation.Ask,
+		quotation.Timestamp,
+		quotation.CreateDate,
+	)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
