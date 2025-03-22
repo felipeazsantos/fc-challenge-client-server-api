f252bf925e73ec947c7a924020c5647cfa7e3fc1
felipeazsantos <felipeasantos10@hotmail.com>
2025-03-22 16:19:00 -0300

    making request by client

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/felipeazsantos/fc-challenge-client-server-api/client/internal/getenv"
+	"github.com/felipeazsantos/fc-challenge-client-server-api/client/internal/quotation"
+)
+
+func main() {
+	if err := getenv.LoadConfig(); err != nil {
+		log.Fatalf("error while trying load config: %v", err)
+	}
+
+	err := quotation.MakeRequestOnServer()
+	if err != nil {
+		log.Fatalf("error when trying making quotation request on server: %v", err)
+	}
+
+	log.Println("quotation request was successfully")
 }
